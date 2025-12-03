abe716a14cb853cb9756b2ee652acb55f1b7cfff
AlSegrarp <163333582+Alsegurarp@users.noreply.github.com>
2025-12-03 07:46:01 -0600

    new provider

--- lib/providers/favorites_meals_provider.dart
+++ lib/providers/favorites_meals_provider.dart
@@ -1,13 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meal_app/models/meal.dart';
 
-class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
-  // initial data is declared by adding a contructor function
-  FavoriteMealsNotifier() : super([]);
-  // we dont expect any parameters, instead, we use ' : ' and then super(); to reach the parent classs
-  // to super(); we pass our initial data.
+class FavoriteMealsNotifier extends Notifier<List<Meal>> {
+  @override
+  List<Meal> build() {
+    return [];
+  }
 
-  // we add any methods we want to modify this data
   void toggleMealFavoriteStatus(Meal meal) {
     final isFavorite = state.contains(meal);
 
@@ -20,6 +19,6 @@ class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
 }
 
 final favoritesMealsProvider =
-    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>(
-      (ref) => FavoriteMealsNotifier(),
+    NotifierProvider<FavoriteMealsNotifier, List<Meal>>(
+      FavoriteMealsNotifier.new,
     );
